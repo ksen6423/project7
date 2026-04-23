dc44abb1be3d3d228e530ae571b2cb24e97082b3
Kseniia Vakhterova <vlsen23@list.ru>
2026-04-23 16:46:07 +0300

    logging masks utils

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,24 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/masks.log", "a", encoding="utf-8")
+formatter = logging.Formatter('%(asctime)s %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция, которая маскирует номер банковской карты"""
     result = ""
     if len(card_number) != 16:
         result = "Номер карты должен содержать 16 цифр"
+        logger.info("Пользователь указал меньше цифр, чем нужно")
     else:
         result = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     return result
 
-print(get_mask_card_number(card_number="2548786214555689"))
+print(get_mask_card_number(card_number="2542586214555689"))
 
 
 def get_mask_account(account_number: str) -> str:
@@ -16,6 +26,7 @@ def get_mask_account(account_number: str) -> str:
     result = ""
     if len(account_number) != 20:
         result = "Номер счета должен содержать 20 цифр"
+        logger.info("Пользователь указал больше цифр, чем нужно")
     else:
         result = f"**{account_number[-4:]}"
     return result
--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,36 @@
-import os
 import json
+import logging
+from typing import List
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
+file_handler = logging.FileHandler("../logs/utils.log", "a", encoding="utf-8")
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 
-def read_json_file(file_path: str) -> list:
+
+def read_json_file(file_path: str) -> List:
     """
       Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
      Если файл пустой, содержит не список или не найден, функция возвращает пустой список.
     """
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             transactions_list = json.load(file)
             if isinstance(transactions_list, list):
+                logger.info(f"Файл {file_path} успешно прочитан.")
                 return transactions_list
-    except (FileNotFoundError, json.JSONDecodeError):
-        pass
+            else:
+                logger.warning(f"Содержимое файла {file_path} не является списком.")
+    except FileNotFoundError:
+        logger.error(f"Файл {file_path} не найден.")
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка декодирования JSON в файле {file_path}.")
     return []
-
-
-
-
-
-
-
+# translations_ = read_json_file('data/operation.json')
+# print(translations_)
 
